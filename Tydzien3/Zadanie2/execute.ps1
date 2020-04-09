b31012c621bef1cdefa747b7ec26f5d78c71859c
MarekW <50923522+MWolosiewicz@users.noreply.github.com>
2020-04-09 20:14:26 +0200

    adjustment

--- Tydzien3/Zadanie2/execute.ps1
+++ Tydzien3/Zadanie2/execute.ps1
@@ -1,3 +1,5 @@
 Connect-AzAccount
+$location = 'koreasouth'
 $resourceGroupName = Read-Host -Prompt "Enter the Resource Group name"
+New-AzResourceGroup -Name 'Tydzien3' -Location $location
 New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateUri https://github.com/MWolosiewicz/AZ_Course/blob/master/Tydzien_3/Zadanie_2/linked.json
